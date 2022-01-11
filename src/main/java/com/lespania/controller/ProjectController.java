edfa871e62e4da46dd58087e374d15241c5e81c0
ececi <huseyinkiceci@gmail.com>
2022-01-11 11:05:22 +0300

    readAll method added to ProjectController

--- src/main/java/com/lespania/controller/ProjectController.java
+++ src/main/java/com/lespania/controller/ProjectController.java
@@ -1,12 +1,18 @@
 package com.lespania.controller;
 
+import com.lespania.annotation.DefaultExceptionMessage;
 import com.lespania.dto.ProjectDTO;
 import com.lespania.dto.TaskDTO;
 import com.lespania.dto.UserDTO;
+import com.lespania.entity.ResponseWrapper;
 import com.lespania.enums.Status;
 import com.lespania.service.ProjectService;
 import com.lespania.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +20,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
-@RequestMapping("/project")
+@RestController
+@RequestMapping("/api/v1/project")
+@Tag(name = "Project Controller",description = "Project API")
 public class ProjectController {
 
     private ProjectService projectService;
@@ -24,6 +31,15 @@ public class ProjectController {
     public ProjectController(ProjectService projectService, UserService userService) {
         this.projectService = projectService;
         this.userService = userService;
+    }
+
+    @GetMapping
+    @Operation(summary = "Read all projects")
+    @DefaultExceptionMessage(defaultMessage = "Something went wrong, try again!")
+    @PreAuthorize("hasAnyAuthority('Admin','Manager')")
+    public ResponseEntity<ResponseWrapper> readAll(){
+        List<ProjectDTO> projectDTOS = projectService.listAllProjects();
+        return ResponseEntity.ok(new ResponseWrapper("Project are retrieved",projectDTOS));
     }
 
 }
