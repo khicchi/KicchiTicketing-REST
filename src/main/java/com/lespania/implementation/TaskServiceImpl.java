4b25e3da244b33588abbe1cb43f9b7362a7311a1
ececi <huseyinkiceci@gmail.com>
2022-01-11 12:46:58 +0300

    task service impl update method updated

--- src/main/java/com/lespania/implementation/TaskServiceImpl.java
+++ src/main/java/com/lespania/implementation/TaskServiceImpl.java
@@ -58,16 +58,12 @@ public class TaskServiceImpl implements TaskService {
     }
 
     @Override
-    public void update(TaskDTO dto) {
-        Optional<Task> task = taskRepository.findById(dto.getId());
-        Task convertedTask = taskMapper.convertToEntity(dto);
-
-        if(task.isPresent()){
-            convertedTask.setId(task.get().getId());
-            convertedTask.setTaskStatus(task.get().getTaskStatus());
-            convertedTask.setAssignedDate(task.get().getAssignedDate());
-            taskRepository.save(convertedTask);
-        }
+    public TaskDTO update(TaskDTO dto) throws TicketingProjectException {
+        taskRepository.findById(dto.getId()).orElseThrow(() ->
+                        new TicketingProjectException("Task does not exists"));
+        Task convertedTask = mapperUtil.convert(dto,new Task());
+        Task save = taskRepository.save(convertedTask);
+        return mapperUtil.convert(save,new TaskDTO());
     }
 
     @Override
