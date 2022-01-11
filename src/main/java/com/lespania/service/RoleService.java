8f0dd24678fc6e98f535ce282401c447e20baf50
ececi <huseyinkiceci@gmail.com>
2022-01-11 16:07:12 +0300

    RoleService updated

--- src/main/java/com/lespania/service/RoleService.java
+++ src/main/java/com/lespania/service/RoleService.java
@@ -1,6 +1,7 @@
 package com.lespania.service;
 
 import com.lespania.dto.RoleDTO;
+import com.lespania.exception.TicketingProjectException;
 
 import java.util.List;
 
@@ -8,5 +9,5 @@ public interface RoleService {
 
     List<RoleDTO> listAllRoles();
 
-    RoleDTO findById(Long id);
+    RoleDTO findById(Long id) throws TicketingProjectException;
 }
