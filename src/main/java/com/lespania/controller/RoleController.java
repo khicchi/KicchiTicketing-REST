dbf9b6a2aca1477635a9e1bb530f2e356fd99758
ececi <huseyinkiceci@gmail.com>
2022-01-11 16:00:15 +0300

    readAll added to RoleController

--- src/main/java/com/lespania/controller/RoleController.java
+++ src/main/java/com/lespania/controller/RoleController.java
@@ -1,9 +1,18 @@
 package com.lespania.controller;
 
+import com.lespania.annotation.DefaultExceptionMessage;
+import com.lespania.dto.RoleDTO;
+import com.lespania.entity.ResponseWrapper;
 import com.lespania.service.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/role")
@@ -15,5 +24,13 @@ public class RoleController {
         this.roleService = roleService;
     }
 
+    @GetMapping
+    @DefaultExceptionMessage(defaultMessage = "Something went wrong,please try again!")
+    @Operation(summary = "Read all roles")
+    @PreAuthorize("hasAuthority('Admin')")
+    public ResponseEntity<ResponseWrapper> readAll(){
+        List<RoleDTO> roleDTOList = roleService.listAllRoles();
+        return ResponseEntity.ok(new ResponseWrapper("Successfully retrieved all roles",roleDTOList));
 
+    }
 }
